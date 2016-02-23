9043a2367b3cef84729ae72c97eb84855dc8fafa
Robin Tweedie <robin@songkick.com>
2016-02-23 17:45:25 +0000

    implement a means to do dry runs without SDB

--- songslack.go
+++ songslack.go
@@ -1,13 +1,18 @@
-/* simple golang program to process Songkick "interested"/"going" events and
-   post them to a given Slack channel
+/*
+	simple golang program to process Songkick "interested"/"going" events and
+	post them to a given Slack channel
 
 	Run with the following environment variables set:
 
-	SONGKICK API KEY=   your songkick api key
-	SLACK API KEY=			your slack api token
+	SONGKICK_API_KEY=   your songkick api key
+	SLACK_API_KEY=			your slack api token
 	SLACK_CHANNEL_NAME= the name of the slack channel you want to post your Songkick events to
-	AWS KEY=						your amazon web services access key
-	AWS SECRET=         your amazon web services secret
+	AWS_KEY=						your amazon web services access key
+	AWS_SECRET=         your amazon web services secret
+
+	to specify a pair of usernames to do a dry-run of posts with, set
+	DRY_RUN_SLACK_USER=
+	DRY_RUN_SONGKICK_USER=
 */
 
 package main
@@ -31,41 +36,58 @@ type SongSlackUser struct {
 
 var (
 	err error
+
 	// Slack
 	slackApiKey  = os.Getenv("SLACK_API_KEY")
 	channelName  = os.Getenv("SLACK_CHANNEL_NAME")
 	slackClient  = slack.New(slackApiKey)
 	slackChannel *slack.Channel
+
 	// AWS
 	mysdb             sdb.SimpleDB
 	myDomain          sdb.Domain
-	aws_access_key    = os.Getenv("AWS_KEY")
-	aws_access_secret = os.Getenv("AWS_SECRET")
+	awsAccessKey    = os.Getenv("AWS_KEY")
+	awsAccessSecret = os.Getenv("AWS_SECRET")
+	sdbKeyFormats = map[string]string{
+		songkick.AttendanceImGoing:  "%s-going-%d",
+		songkick.AttendanceIMightGo: "%s-tracking-%d"}
 
 	messages = map[string]string{
 		songkick.AttendanceImGoing:  "I’m going to",
 		songkick.AttendanceIMightGo: "I’m interested in"}
-	sdbKeyFormats = map[string]string{
-		songkick.AttendanceImGoing:  "%s-going-%d",
-		songkick.AttendanceIMightGo: "%s-tracking-%d"}
 	users = []SongSlackUser{}
+
+	dryRunUser = SongSlackUser{os.Getenv("DRY_RUN_SONGKICK_USER"), os.Getenv("DRY_RUN_SLACK_USER")}
 )
 
-func init() {
-	awsKey := aws.AccessKey{aws_access_key, aws_access_secret}
+func isDryRun() bool {
+	if dryRunUser.SongkickUsername != "" && dryRunUser.SlackUsername != "" {
+		return true
+	} else {
+		return false
+	}
+}
 
+func init() {
+	if isDryRun() {
+		users = append(users, dryRunUser)
+	}
 	// add your Songkick and Slack usernames here we check for new events for
 	// each Songkick user and post it to the a Slack channel for the
 	// corresponding Slack user (manually specified in this example, you could of
 	// course read this from elsewhere)
-	users = append(users, SongSlackUser{"songkick_username_1", "slack_username_1"})
-	users = append(users, SongSlackUser{"songkick_username_2", "slack_username_2"})
+	if !isDryRun() {
+		users = append(users, SongSlackUser{"songkick_username_1", "slack_username_1"})
+		users = append(users, SongSlackUser{"songkick_username_2", "slack_username_2"})
 
-	// connect to SimpleDB, to store previously posted events you could replace
-	// this with the datastore of your choice
-	mysdb, _ = sdb.NewSimpleDB(sdb.RegionEuIreland, awsKey)
-	// the domain that we want to use with simple db to store our data
-	myDomain, _ = mysdb.OpenDomain("songslack")
+		awsKey := aws.AccessKey{awsAccessKey, awsAccessSecret}
+
+		// connect to SimpleDB, to store previously posted events you could replace
+		// this with the datastore of your choice
+		mysdb, _ = sdb.NewSimpleDB(sdb.RegionEuIreland, awsKey)
+		// the domain that we want to use with simple db to store our data
+		myDomain, _ = mysdb.OpenDomain("songslack")
+	}
 
 	slackChannel, err = slackClient.FindChannelByName(channelName)
 	if err != nil {
@@ -84,25 +106,31 @@ func main() {
 			for _, songkickEvent := range jsonData.ResultsPage.ResultData.EventData {
 				sdbKey := fmt.Sprintf(sdbKeyFormats[attendanceType], u.SongkickUsername, songkickEvent.ID)
 
-				sItem, _, err := mysdb.Select("select * from songslack where itemName() = '"+sdbKey+"'", true, nil)
-				if err != nil {
-					panic(err)
+				var sItem []sdb.SelectedItem
+
+				if !isDryRun() {
+					sItem, _, err = mysdb.Select("select * from songslack where itemName() = '"+sdbKey+"'", true, nil)
+					if err != nil {
+						panic(err)
+					}
 				}
-				// if no match, hasn't been posted before so lets send it to Slack
+
+				// if there is no match (hasn't been posted before), let's send it to Slack
 				if len(sItem) == 0 {
 					err = postToSlack(u.SlackUsername, attendanceType, songkickEvent)
 					if err != nil {
 						panic(err)
 					}
-					os.Exit(0)
 
-					// add the item to sdb
-					newItem := sdb.ItemName(sdbKey)
-					upd := make([]sdb.PutUpdate, 1, 1)
-					upd[0] = sdb.PutUpdate{"eventid", strconv.Itoa(songkickEvent.ID), true}
-					err = myDomain.PutAttributes(newItem, upd, nil)
-					if err != nil {
-						panic(err)
+					if !isDryRun() {
+						// add the item to sdb
+						newItem := sdb.ItemName(sdbKey)
+						upd := make([]sdb.PutUpdate, 1, 1)
+						upd[0] = sdb.PutUpdate{"eventid", strconv.Itoa(songkickEvent.ID), true}
+						err = myDomain.PutAttributes(newItem, upd, nil)
+						if err != nil {
+							panic(err)
+						}
 					}
 				}
 			} // end of for _, songkickEvent := range jsonData...
@@ -111,7 +139,6 @@ func main() {
 }
 
 func postToSlack(username string, attendanceType string, songkickEvent songkick.Event) error {
-
 	messageBase := messages[attendanceType]
 	message := messageBase + "..."
 
